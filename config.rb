9f7dc44c4387103f7a583db013c8ec5be9d48b5e
Shaun O'Connell <shaun.oconnell@abletech.co.nz>
2016-03-08 11:14:28 +1300

    Bump Ruby and Middleman to latest supported versions. Remove the useless specs.

--- config.rb
+++ config.rb
@@ -1,4 +1,4 @@
-env_name = ENV['MM_ENV'] || 'development'
+env_name = ENV['APP_ENV'] || 'development'
 require "config/environments/#{env_name}"
 require 'helpers/asset_helpers'
 
@@ -42,7 +42,7 @@ page "/sitemap.xml", :layout => false
 
 # Reload the browser automatically whenever files change
 configure :development do
-  activate :livereload
+	activate :livereload
 end
 
 # Methods defined in the helpers block are available in templates
@@ -63,63 +63,77 @@ activate :directory_indexes
 activate :gzip
 
 activate :autoprefixer do |config|
-  config.browsers = ['last 2 versions', 'Explorer >= 9']
-  config.cascade  = false
+	config.browsers = ['last 2 versions', 'Explorer >= 9']
+	config.cascade  = false
 end
 
 # Build-specific configuration
 configure :build do
 
-  # For example, change the Compass output style for deployment
-  activate :minify_css
+	activate :gzip do |gzip|
+		gzip.exts = %w(.js .css .html .htm .svg)
+	end
 
-  # Minify Javascript on build
-  activate :minify_javascript
+	# For example, change the Compass output style for deployment
+	activate :minify_css
 
-  # Enable cache buster
-  activate :asset_hash, :ignore => [/blog/, /touch-icon/, /opengraph/]
+	# Minify Javascript on build
+	activate :minify_javascript
+	set :js_compressor, Uglifier.new(:mangle => false)
 
-  activate :minify_html do |html|
-    html.remove_http_protocol = false
-  end
+	# Enable cache buster
+	activate :asset_hash, :ignore => [/blog/, /touch-icon/, /opengraph/]
 
-  # Use relative URLs
-  # activate :relative_assets
+	activate :minify_html do |html|
+		html.remove_http_protocol    = false
+		html.remove_input_attributes = false
+		html.remove_quotes           = false
+		html.remove_intertag_spaces  = true
+	end
 
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+	# Use relative URLs
+	# activate :relative_assets
+
+	# Or use a different image path
+	# set :http_prefix, "/Content/images/"
 end
 
 if ApplicationConfig.const_defined?(:S3)
-  activate :s3_sync do |s3_sync|
-    s3_sync.bucket                     = ApplicationConfig::S3::BUCKET # The name of the S3 bucket you are targeting. This is globally unique.
-    s3_sync.region                     = 'ap-southeast-2'     # The AWS region for your bucket.
-    s3_sync.aws_access_key_id          = ApplicationConfig::S3::ACCESS_ID
-    s3_sync.aws_secret_access_key      = ApplicationConfig::S3::SECRET_KEY
-    s3_sync.delete                     = false # We delete stray files by default.
-    s3_sync.after_build                = false # We do not chain after the build step by default.
-    s3_sync.prefer_gzip                = true
-    s3_sync.path_style                 = true
-    s3_sync.reduced_redundancy_storage = false
-    s3_sync.acl                        = 'public-read'
-    s3_sync.encryption                 = false
-  end
+	activate :s3_sync do |s3_sync|
+		s3_sync.bucket                     = ApplicationConfig::S3::BUCKET # The name of the S3 bucket you are targeting. This is globally unique.
+		s3_sync.region                     = 'ap-southeast-2'     # The AWS region for your bucket.
+		s3_sync.aws_access_key_id          = ApplicationConfig::S3::ACCESS_ID
+		s3_sync.aws_secret_access_key      = ApplicationConfig::S3::SECRET_KEY
+		s3_sync.delete                     = false # We delete stray files by default.
+		s3_sync.after_build                = false # We do not chain after the build step by default.
+		s3_sync.prefer_gzip                = true
+		s3_sync.path_style                 = true
+		s3_sync.reduced_redundancy_storage = false
+		s3_sync.acl                        = 'public-read'
+		s3_sync.encryption                 = false
+	end
 
-  caching_policy 'text/css', max_age: (60 * 60 * 24 * 365), public: true
-  caching_policy 'application/javascript', max_age: (60 * 60 * 24 * 365), public: true
-  caching_policy 'image/png', max_age: (60 * 60 * 24 * 365), public: true
-  caching_policy 'image/jpeg', max_age: (60 * 60 * 24 * 365), public: true
-  caching_policy 'image/x-icon', max_age: (60 * 60 * 24 * 365), public: true
-  # caching_policy 'text/html', max_age: (60 * 60 * 2), public: true
+	caching_policy 'text/css',               max_age: (60 * 60 * 24 * 365), public: true
+	caching_policy 'application/javascript', max_age: (60 * 60 * 24 * 365), public: true
+	caching_policy 'image/gif',              max_age: (60 * 60 * 24 * 365), public: true
+	caching_policy 'image/png',              max_age: (60 * 60 * 24 * 365), public: true
+	caching_policy 'image/jpeg',             max_age: (60 * 60 * 24 * 365), public: true
+	caching_policy 'image/x-icon',           max_age: (60 * 60 * 24 * 365), public: true
+	caching_policy 'image/svg+xml',          max_age: (60 * 60 * 24 * 365), public: true
+	caching_policy 'application/font-woff',  max_age: (60 * 60 * 24 * 365), public: true
+	caching_policy 'application/font-woff2', max_age: (60 * 60 * 24 * 365), public: true
+	caching_policy 'font/woff',              max_age: (60 * 60 * 24 * 365), public: true
+	caching_policy 'font/woff2',             max_age: (60 * 60 * 24 * 365), public: true
+	caching_policy 'text/html',              max_age: (60 * 5), public: true
 
-  activate :cloudfront do |cf|
-    cf.access_key_id = ApplicationConfig::S3::ACCESS_ID
-    cf.secret_access_key = ApplicationConfig::S3::SECRET_KEY
-    cf.distribution_id = ApplicationConfig::S3::CLOUDFRONT_DIST_ID
-    cf.filter = /\.html$/i
-  end
+	activate :cloudfront do |cf|
+		cf.access_key_id = ApplicationConfig::S3::ACCESS_ID
+		cf.secret_access_key = ApplicationConfig::S3::SECRET_KEY
+		cf.distribution_id = ApplicationConfig::S3::CLOUDFRONT_DIST_ID
+		cf.filter = /\.html$/i
+	end
 
-  after_s3_sync do |files_by_status|
-    invalidate(files_by_status[:updated] + files_by_status[:created] + files_by_status[:deleted])
-  end
+	after_s3_sync do |files_by_status|
+		invalidate(files_by_status[:updated] + files_by_status[:created] + files_by_status[:deleted])
+	end
 end
