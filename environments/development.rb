4ba974a1ab20e96780ea25833d38d449af88dfa6
Shaun O'Connell <shaun.oconnell@abletech.co.nz>
2016-07-08 16:59:33 +1200

    Switch off service-workers in our local dev environments.

--- environments/development.rb
+++ environments/development.rb
@@ -1,7 +1,7 @@
 module ApplicationConfig
   BASE_URL = 'http://localhost:4567'
 
-  SERVICE_WORKER_ENABLED = ENV['SW_ENABLED'] || true
+  SERVICE_WORKER_ENABLED = ENV['SW_ENABLED'] || false
 
   module GoogleAnalytics
     TRACKING_KEY = 'UA-7731138-1'
